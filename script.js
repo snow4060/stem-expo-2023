7b7471cd63165902ecc254911c6b37d39156bfd9
snow4060 <luohaoyun4068@students.esuhsd.org>
2023-06-12 00:08:06 -0700

    panelists, schedule, sponsors

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
+var pages;
+async function loadPages(){
+    pages = document.querySelectorAll('.container');
+    console.log(pages);
+    pages[0].classList.add('focus');
+    pages[0].style.top = '0';
+    for(let i = 1; i < pages.length; i++){
+        pages[i].style.top = '100vh';
+    }
+}
+
+
+
+
+
 document.addEventListener("DOMContentLoaded", async function() {
+    loadPages();
     // Get the animated-text element
     const animatedText = document.querySelector(".large-text");
     
@@ -19,39 +35,121 @@ document.addEventListener("DOMContentLoaded", async function() {
     setTimeout(() => {
             document.querySelector('.small-text').style.visibility = 'visible';
     }, characters.length*100 + 150);
-
-    console.log(document.querySelector('.small-text').innerHTML);
-
 });
 
-var firstDiv = document.querySelector('.logo-background');
-var secondDiv = document.querySelector('.purpose');
-secondDiv.style.top = '100vh';
-  
-window.addEventListener('wheel', function(event) {
-    event.preventDefault();
-    var scrollAmt = event.deltaY*300;
-    var top = parseFloat(secondDiv.style.top.slice(0, -2));
-    var newTop = top - scrollAmt/window.innerHeight;
+function scroll(div1, div2, amt){
+    var top = parseFloat(div2.style.top.slice(0, -2));
+    var newTop = top - amt/window.innerHeight;
     let j = 0;
     for(let i = 0; Math.abs(i) < Math.abs(newTop-top); i+= (newTop-top)/20){
         j++;
         setTimeout(() => {
             if((top+i) >= 100){
-                secondDiv.style.top = '100vh';
+                div2.style.top = '100vh';
+                div1.classList.add('focus');
+                div2.classList.remove('focus');
+                div2.classList.remove('progress');
+                return;
             }
             else if((top+i) <= 0){
-                secondDiv.style.top = '0vh';
+                div2.style.top = '0vh';
+                div1.classList.remove('focus');
+                div2.classList.add('focus');
+                div2.classList.remove('progress');
+                return;
             }
             else{
             
-            secondDiv.style.top = (top + i) + 'vh';
-            firstDiv.style.filter = 'brightness(' + (parseFloat(secondDiv.style.top.slice(0, -2))) + '%)';
-            console.log(firstDiv.style.filter);
+            div2.style.top = (top + i) + 'vh';
+            div1.style.filter = 'brightness(' + (parseFloat(div2.style.top.slice(0, -2))) + '%)';            div2.classList.add('progress');
 
             }
         }, j*6);
-        
+            
+    }
+}
+
+var firstDiv, secondDiv;
+
+
+window.addEventListener('wheel', function(event) {
+    if(pages[2].style.top === '0vh'){
+        pages[2].style.overflowY = 'scroll';
+    }
+    else{
+        pages[2].style.overflowY = 'hidden';
+    }
+    //positive: scroll down
+    if(event.deltaY > 0){
+        event.preventDefault();        
+        //find the page that is in focus
+        for(let i = 0; i < pages.length; i++){
+            if(pages[i].classList.contains('focus')){
+                firstDiv = pages[i];
+                secondDiv = pages[i+1];
+            }
+            if(pages[i].classList.contains('progress')){
+                firstDiv = pages[i-1];
+                secondDiv = pages[i];
+                break;
+            }
+        }
+        //prevent overflow
+        if(Array.prototype.indexOf.call(pages, firstDiv)+1 >= pages.length || Array.prototype.indexOf.call(pages, secondDiv) < 1){
+            return;
+        }
+        // console.log(firstDiv.scrollHeight)
+        if(firstDiv.classList.contains('panelist-schedule') && firstDiv.scrollTop + firstDiv.clientHeight < firstDiv.scrollHeight){ 
+            return; 
+        }
+
+        scroll(firstDiv, secondDiv, event.deltaY*300);
+    }
+    // negative: scroll up 
+    else{
+        console.log("triggered")
+        var firstDiv, secondDiv;
+        //find the page that is in focus
+        for(var i = 0; i < pages.length; i++){
+            if(pages[i].classList.contains('focus')){
+                console.log("focus")
+                firstDiv = pages[i-1];
+                secondDiv = pages[i];
+            }
+            if(pages[i].classList.contains('progress')){
+                console.log("progress")
+                firstDiv = pages[i-1];
+                secondDiv = pages[i];
+                break;
+            }
+        }
+        //prevent overflow
+        if(Array.prototype.indexOf.call(pages, secondDiv) < 1){ return; }
+        if(secondDiv.classList.contains('panelist-schedule') && secondDiv.scrollTop > 0){ return; }
+        scroll(firstDiv, secondDiv, event.deltaY*300);
     }
 });
   
+
+var panelistsContainer = document.getElementById('panelists');
+var panelistsContainerBack = document.getElementById('panelist-back');
+
+panelistsContainerBack.style.height = panelistsContainer.clientHeight + 'px';
+
+window.addEventListener('resize', function(){
+    panelistsContainerBack.style.height = panelistsContainer.clientHeight + 'px';
+})
+
+window.addEventListener('load', function(){
+    var rows = document.querySelectorAll(".schedule-row");
+    var tallestHeight = 0;
+    rows.forEach(function(row){
+        if(row.offsetHeight > tallestHeight){
+            tallestHeight = row.offsetHeight;
+        }
+    })
+    console.log(tallestHeight)
+    rows.forEach(function(row){
+        row.style.height = tallestHeight + 'px';
+    })
+})
