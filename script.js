8b88da1b9c87a5c8964cefae0ad4414049246f50
snow4060 <luohaoyun4068@students.esuhsd.org>
2023-06-15 09:24:12 -0700

    reworked scrolling, added keyboard support

--- script.js
+++ script.js
@@ -38,37 +38,52 @@ document.addEventListener("DOMContentLoaded", async function() {
     }, characters.length*100 + 150);
 });
 
-function scroll(div1, div2, amt){
+function wait(timeout){
+    return new Promise(function(resolve) {
+        setTimeout(resolve, timeout);
+    });
+}
+
+async function scroll(div1, div2, amt, full=false){
+    console.log("scroll");
     var top = parseFloat(div2.style.top.slice(0, -2));
-    var newTop = top - 30*Math.sign(amt);
-    let j = 0;
-    for(let i = 0; Math.abs(i) < 30; i+= (newTop-top)/30){
-        j++;
-        setTimeout(() => {
-            if((top+i) >= 100){
-                div2.style.top = '100vh';
-                div1.classList.add('focus');
-                div2.classList.remove('focus');
-                div2.classList.remove('progress');
-                pauseScroll();
-                return;
-            }
-            else if((top+i) <= 0){
-                div2.style.top = '0vh';
-                div1.classList.remove('focus');
-                div2.classList.add('focus');
-                div2.classList.remove('progress');
-                pauseScroll();
-                return;
-            }
-            else{
-            
+    
+    if(full && amt > 0){
+        var newTop = 0;
+    }
+    else if(full && amt < 0){
+        var newTop = 100;
+    }
+    else{
+        var newTop = top - 10*Math.sign(amt);
+    }
+    for(let i = 1; Math.abs(i) <= Math.abs(newTop-top); i+= 1*Math.sign(newTop-top)){
+        console.log("e");
+        if((top+i) >= 100 && amt < 0){
+            div2.style.top = '100vh';
+            div1.classList.add('focus');
+            div2.classList.remove('focus');
+            div2.classList.remove('progress');
+            pauseScroll();
+            console.log("case 1");
+            break;
+        }
+        else if((top+i) <= 0 && amt > 0){
+            div2.style.top = '0vh';
+            div1.classList.remove('focus');
+            div2.classList.add('focus');
+            div2.classList.remove('progress');
+            pauseScroll();
+            console.log("case 2");
+            break;
+        }
+        else{
             div2.style.top = (top + i) + 'vh';
             div1.style.filter = 'brightness(' + (parseFloat(div2.style.top.slice(0, -2))) + '%)';            
             div2.classList.add('progress');
-
-            }
-        }, j*6);
+            console.log("scrolling");
+            await wait(1);
+        }
             
     }
 }
@@ -82,60 +97,95 @@ organizerContainer.style.top = '100vh';
 var clubsIconsContainer = document.querySelector('.clubs-icons-container');
 clubsIconsContainer.style.marginLeft = '80%';
 
-function scrollLast(event){
+async function scrollLast(event, full=false){
     console.log("scroll last: " + event.deltaY);
     // var scrollAmt = event.deltaY*150;
     var top = parseFloat(organizerContainer.style.top.slice(0, -2));
-    var newTop = top - 15*Math.sign(event.deltaY);
-    let j = 0;
-    for(let i = 0; Math.abs(i) < 15; i += (newTop-top)/15){
-        j++;
-        setTimeout(() => {
-            if((top+i) >= 100){
-                clubsContainer.style.top = '100vh';
-                organizerContainer.style.top = '100vh';
-                clubsIconsContainer.style.marginLeft = '80%';
-                sponsors.classList.add('focus');
-                organizerContainer.classList.remove('focus');
-                organizerContainer.classList.remove('progress');
-                return;
-            }
-            else if((top+i) <= 50){
-                organizerContainer.style.top = '50vh';
-                clubsContainer.style.opacity = 1;
-                clubsIconsContainer.style.marginLeft = '0';
-                sponsors.classList.remove('focus');
-                organizerContainer.classList.add('focus');
-                organizerContainer.classList.remove('progress');
-                return;
-            }
-            else{
-                // clubsBack.style.filter = 'brightness(' + Math.abs((1 - (top+i)/50)*100) + '%)';
-                clubsContainer.style.top = '0';
-                clubsContainer.style.opacity = Math.abs((100-top-i)/50);
-                clubsIconsContainer.style.marginLeft = (Math.abs(top+i-50)/50*80) + '%';
-                console.log((Math.abs(top+i-50)/50)*80)
-                organizerContainer.style.top = (top+i) + 'vh';
-                organizerContainer.classList.add('progress');
-            }
-        }, j*6)
+    var newTop = top - 17*Math.sign(event.deltaY);
+    if(full && event.deltaY > 0){
+        newTop = 100;
+    }
+    else if(full && event.deltaY < 0){
+        newTop = 50;
+    }
+    for(let i = 1; Math.abs(i) <= Math.abs(newTop-top); i += 1*Math.sign(newTop-top)){
+        if((top+i) >= 100 && event.deltaY < 0){
+            clubsContainer.style.top = '100vh';
+            organizerContainer.style.top = '100vh';
+            clubsIconsContainer.style.marginLeft = '80%';
+            sponsors.classList.add('focus');
+            organizerContainer.classList.remove('focus');
+            organizerContainer.classList.remove('progress');
+            return;
+        }
+        else if((top+i) <= 50 && event.deltaY > 0){
+            organizerContainer.style.top = '50vh';
+            clubsContainer.style.opacity = 1;
+            clubsIconsContainer.style.marginLeft = '0';
+            sponsors.classList.remove('focus');
+            organizerContainer.classList.add('focus');
+            organizerContainer.classList.remove('progress');
+            return;
+        }
+        else{
+            // clubsBack.style.filter = 'brightness(' + Math.abs((1 - (top+i)/50)*100) + '%)';
+            clubsContainer.style.top = '0';
+            clubsContainer.style.opacity = Math.abs((100-top-i)/50);
+            clubsIconsContainer.style.marginLeft = (Math.abs(top+i-50)/50*80) + '%';
+            console.log((Math.abs(top+i-50)/50)*80)
+            organizerContainer.style.top = (top+i) + 'vh';
+            organizerContainer.classList.add('progress');
+            await wait(1);
+        }
     }
 }
 
-function handleScroll(event) {
+async function scrollPage(event, full=false){
+    console.log("scroll page function call");
+    var top = pages[2].scrollTop;
+    var delta = pages[2].clientHeight*0.1*Math.sign(event.deltaY);
+    if(full && event.deltaY > 0){
+        delta = pages[2].scrollHeight - top;
+    }
+    else if(full && event.deltaY < 0){
+        delta = -top-2;
+    }
+    console.log("delta: " + delta);
+    for(let i = 1; Math.abs(i) <= Math.abs(delta); i += delta*0.1){
+        if(pages[2].scrollTop + pages[2].clientHeight >= pages[2].scrollHeight && event.deltaY > 0){
+            pages[2].scrollTop = pages[2].scrollHeight;
+            console.log("case 1");
+            pauseScroll();
+            return;
+        }
+        else if(pages[2].scrollTop <= 0 && event.deltaY < 0){
+            pages[2].scrollTop = 0;
+            console.log("case 2");
+            pauseScroll();
+            return;
+        }
+        else{
+            console.log("attempted scrolling");
+            pages[2].scrollTop = top+i;
+            console.log(pages[2].scrollTop);
+            await wait(1);
+        }
+    }
+}
 
+function handleScroll(event, full=false) {
     //whether allow scrolling on panelist page 
-    if(parseFloat(pages[2].style.top.slice(0, -2)) < 1){
-        pages[2].style.overflowY = 'scroll';
+    if(parseFloat(pages[2].style.top.slice(0, -2)) == 0 && pages[3].style.top.slice(0, -2) >= 100){
+        console.log("scroll top: " + pages[2].scrollTop);
+        console.log("client height: " + pages[2].clientHeight);
+        console.log("scroll height: " + pages[2].scrollHeight);
+        scrollPage(event, full);
     }
-    else{
-        pages[2].style.overflowY = 'hidden';
-    }
+
     //positive: scroll down
     if(event.deltaY > 0){
         if(sponsors.classList.contains('focus') && !sponsors.classList.contains('progress')){
-            // scrollLast(event);
-            scrollLast(event);
+            scrollLast(event, full);
             return;   
         }       
         //find the page that is in focus
@@ -160,12 +210,13 @@ function handleScroll(event) {
             console.log(firstDiv.style.overflowY);
             return; 
         }
-        scroll(firstDiv, secondDiv, event.deltaY);
+        console.log("normal scroll call");
+        scroll(firstDiv, secondDiv, event.deltaY, full);
     }
     // negative: scroll up 
     else{
         if(organizerContainer.classList.contains('focus') || organizerContainer.classList.contains('progress')){
-            scrollLast(event);
+            scrollLast(event, full);
             return;
         }
         //find the page that is in focus
@@ -184,7 +235,7 @@ function handleScroll(event) {
         if(Array.prototype.indexOf.call(pages, secondDiv) < 1){ return; }
         if(secondDiv.classList.contains('panelist-schedule') && secondDiv.scrollTop > 0){ return; }
 
-        scroll(firstDiv, secondDiv, event.deltaY);
+        scroll(firstDiv, secondDiv, event.deltaY, full);
     }
 }
 
@@ -197,7 +248,7 @@ function pauseScroll(){
 }
 
 var firstDiv, secondDiv;
-window.addEventListener('wheel', handleScroll);
+window.addEventListener('wheel', handleScroll, { passive: false });
 
 
 var panelistsContainer = document.getElementById('panelists');
@@ -318,15 +369,16 @@ document.body.addEventListener('click', function(event){
 
 document.addEventListener('keydown', function(event){
     if(event.key === 'ArrowUp'){
-        console.log('up');
+        handleScroll({deltaY: -1});
     }
     else if(event.key === 'ArrowDown'){
-        console.log('down');
+        handleScroll({deltaY: 1});
     }
     else if(event.key === 'PageUp'){
-        console.log('page up');
+        handleScroll({deltaY: -1}, true);
     }
     else if(event.key === 'PageDown'){
         console.log('page down');
+        handleScroll({deltaY: 1}, true);
     }
 })
